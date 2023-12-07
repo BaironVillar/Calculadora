d730acb09bcaf20237f625c26adf1a2617a1eaef
Baion Villar <baironvillarberrios@gmail.con>
2023-12-07 16:58:48 -0300

    Actualizacion general: V1.0

--- Calculadora Triangulos Rectangulos.py
+++ Calculadora Triangulos Rectangulos.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 root = Tk()
 root.title("Calculadora Triangulos Rectangulos")
@@ -30,9 +31,9 @@ def hipotenusa_page():
         def calculate_hipotenuse():
             cateto1 = float(e1.get())
             cateto2 = float(e2.get())
-            hipotenusa = cateto1/cateto2
+            hipotenusa = math.sqrt(cateto1**2 + cateto2**2)
             e3.delete(0, "end")
-            e3.insert(END, f'{hipotenusa}')
+            e3.insert(END, f'{round(hipotenusa,2)}')
                 
                             # Crear entradas y etiquetas
         label1 = Label(main_frame,font=("Arial black", 14), text="Cateto 1")
@@ -65,9 +66,9 @@ def hipotenusa_page():
         def calculate_cateto():
             cateto1 = float(e1.get())
             hipotenusa = float(e2.get())
-            cateto2 = cateto1/hipotenusa
+            cateto2 = math.sqrt(cateto1**2 + hipotenusa**2)
             e3.delete(0, "end")
-            e3.insert(END, f'{cateto2}')
+            e3.insert(END, f'{round(cateto2,2)}')
             
                             # Crear entradas y etiquetas
         label1 = Label(main_frame,font=("Arial black", 14), text="Cateto 1")
@@ -97,112 +98,291 @@ def seno_page():
     lb = Label(main_frame, text="SENO", font=("Arial black",25))
     lb.place(relx=0.50,rely=0.10,anchor=CENTER)
 
-    def calculate_seno():
-        anguloOpuesto = float(e1.get())
-        hipotenusa = float(e2.get())
-        seno = anguloOpuesto/hipotenusa
-        e3.delete(0, "end")
-        e3.insert(END, f'{seno}')
+    seno_angulo_btn = Button(main_frame, text="CALCULAR SENO SIN ÁNGULO", font=("Arial black", 18), bg="#FF6F61", fg='white',
+                    cursor="hand2",command=lambda: pages(seno_sin_angulo_op))
+    seno_angulo_btn.place(rely=0.35,relx=0.50,width=450, height=100, anchor=CENTER)
 
-    # Crear entradas y etiquetas
-    label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo Opuesto")
-    label1.place(rely=0.27,relx=0.22,anchor=W)
-
-    label2 = Label(main_frame,font=("Arial black", 14), text="Hipotenusa")
-    label2.place(rely=0.42,relx=0.22,anchor=W)
-
-    label3 = Label(main_frame,font=("Arial black", 14), text="Seno")
-    label3.place(rely=0.57,relx=0.22,anchor=W)
-
-    e1 = Entry(main_frame,font=("Arial black", 14))
-    e1.place(rely=0.27,relx=0.78,relheight=0.06,anchor=E)
-
-    e2 = Entry(main_frame,font=("Arial black", 14))
-    e2.place(rely=0.42,relx=0.78,relheight=0.06,anchor=E)
-
-    e3 = Entry(main_frame,font=("Arial black", 14))
-    e3.place(rely=0.57,relx=0.78,relheight=0.06,anchor=E)
-    e3.config()
+    seno_razon_btn = Button(main_frame, text="CALCULAR SENO CON ÁNGULO", font=("Arial black", 18), bg="#FF6F61", fg='white',
+                    cursor="hand2",command=lambda: pages(seno_con_angulo_op))
+    seno_razon_btn.place(rely=0.60,relx=0.50,width=450, height=100, anchor=CENTER)
+    
 
 
-    # Crear botones
-    bt_calculate = Button(main_frame, text="Calcular Seno",cursor="hand2", command=calculate_seno,
-                        font=("Arial black", 18), bg="#FF6F61", fg='white')
-    bt_calculate.place(rely=0.75,relx=0.50, anchor=CENTER) 
+    def seno_sin_angulo_op():
+        lb = Label(main_frame, text="SENO", font=("Arial black",25))
+        lb.place(relx=0.50,rely=0.10,anchor=CENTER)
+
+        def calcular_seno_sin_angulo():
+            catetoOpuesto = float(e1.get())
+            hipotenusa = float(e2.get())
+            seno = catetoOpuesto/hipotenusa
+            e3.delete(0, "end")
+            e3.insert(END, f'{round(seno,2)}')
+
+        formula = Label(main_frame,font=("Arial black", 25), text="Hipotenusa") 
+        formula.place(rely=0.35,relx=0.465,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="________________") 
+        formula.place(rely=0.31,relx=0.435,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Opuesto") 
+        formula.place(rely=0.30,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="sen α =") 
+        formula.place(rely=0.32,relx=0.30,anchor=W)
+
+        # Crear entradas y etiquetas
+        label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo Opuesto")
+        label1.place(rely=0.50,relx=0.22,anchor=W)
+
+        label2 = Label(main_frame,font=("Arial black", 14), text="Hipotenusa")
+        label2.place(rely=0.60,relx=0.22,anchor=W)
+
+        label3 = Label(main_frame,font=("Arial black", 14), text="Seno")
+        label3.place(rely=0.70,relx=0.22,anchor=W)
+
+        e1 = Entry(main_frame,font=("Arial black", 14))
+        e1.place(rely=0.50,relx=0.78,relheight=0.06,anchor=E)
+
+        e2 = Entry(main_frame,font=("Arial black", 14))
+        e2.place(rely=0.60,relx=0.78,relheight=0.06,anchor=E)
+
+        e3 = Entry(main_frame,font=("Arial black", 14))
+        e3.place(rely=0.70,relx=0.78,relheight=0.06,anchor=E)
+        e3.config()
+
+        # Crear botones
+        bt_calculate = Button(main_frame, text="Calcular Seno",cursor="hand2", command=calcular_seno_sin_angulo,
+                            font=("Arial black", 18), bg="#FF6F61", fg='white')
+        bt_calculate.place(rely=0.85,relx=0.50, anchor=CENTER) 
+
+    def seno_con_angulo_op():
+        lb = Label(main_frame, text="SENO", font=("Arial black",25))
+        lb.place(relx=0.50,rely=0.10,anchor=CENTER)
+
+        def calcular_seno_con_angulo():
+            anguloSeno = float(e1.get())
+            seno = math.sin(math.radians(anguloSeno))
+            e2.delete(0, "end")
+            e2.insert(END, f'{round(seno,2)}')
+
+        formula = Label(main_frame,font=("Arial black", 25), text="Hipotenusa") 
+        formula.place(rely=0.40,relx=0.465,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="________________") 
+        formula.place(rely=0.36,relx=0.435,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Opuesto") 
+        formula.place(rely=0.35,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="sen α =") 
+        formula.place(rely=0.37,relx=0.30,anchor=W)
+
+        # Crear entradas y etiquetas
+        label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo del Seno")
+        label1.place(rely=0.60,relx=0.22,anchor=W)
+
+        label2 = Label(main_frame,font=("Arial black", 14), text="Seno")
+        label2.place(rely=0.70,relx=0.22,anchor=W)
+
+        e1 = Entry(main_frame,font=("Arial black", 14))
+        e1.place(rely=0.60,relx=0.78,relheight=0.06,anchor=E)
+
+        e2 = Entry(main_frame,font=("Arial black", 14))
+        e2.place(rely=0.70,relx=0.78,relheight=0.06,anchor=E)
+        e2.config()
+
+        # Crear botones
+        bt_calculate = Button(main_frame, text="Calcular Seno",cursor="hand2", command=calcular_seno_con_angulo,
+                            font=("Arial black", 18), bg="#FF6F61", fg='white')
+        bt_calculate.place(rely=0.85,relx=0.50, anchor=CENTER) 
 
 def coseno_page():
     lb = Label(main_frame, text="COSENO", font=("Arial black",25))
     lb.place(relx=0.50,rely=0.10,anchor=CENTER)
 
-    def calculate_coseno():
-        anguloAdyacente = float(e1.get())
-        hipotenusa = float(e2.get())
-        coseno = anguloAdyacente/hipotenusa
-        e3.delete(0, "end")
-        e3.insert(END, f'{coseno}')
+    seno_angulo_btn = Button(main_frame, text="CALCULAR COSENO SIN ÁNGULO", font=("Arial black", 18), bg="#FF6F61", fg='white',
+                    cursor="hand2",command=lambda: pages(coseno_sin_angulo_op))
+    seno_angulo_btn.place(rely=0.35,relx=0.50,width=500, height=100, anchor=CENTER)
 
-    # Crear entradas y etiquetas
-    label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo Opuesto")
-    label1.place(rely=0.27,relx=0.22,anchor=W)
+    seno_razon_btn = Button(main_frame, text="CALCULAR COSENO CON ÁNGULO", font=("Arial black", 18), bg="#FF6F61", fg='white',
+                    cursor="hand2",command=lambda: pages(coseno_con_angulo_op))
+    seno_razon_btn.place(rely=0.60,relx=0.50,width=500, height=100, anchor=CENTER)
 
-    label2 = Label(main_frame,font=("Arial black", 14), text="Hipotenusa")
-    label2.place(rely=0.42,relx=0.22,anchor=W)
+    def coseno_sin_angulo_op():
+        lb = Label(main_frame, text="COSENO", font=("Arial black",25))
+        lb.place(relx=0.50,rely=0.10,anchor=CENTER)
 
-    label3 = Label(main_frame,font=("Arial black", 14), text="Coseno")
-    label3.place(rely=0.57,relx=0.22,anchor=W)
+        def calcular_coseno_sin_angulo():
+            catetoAdyacente = float(e1.get())
+            hipotenusa = float(e2.get())
+            coseno = catetoAdyacente/hipotenusa
+            e3.delete(0, "end")
+            e3.insert(END, f'{round(coseno,2)}')
 
-    e1 = Entry(main_frame,font=("Arial black", 14))
-    e1.place(rely=0.27,relx=0.78,relheight=0.06,anchor=E)
+        formula = Label(main_frame,font=("Arial black", 25), text="Hipotenusa") 
+        formula.place(rely=0.35,relx=0.465,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="________________") 
+        formula.place(rely=0.31,relx=0.435,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Adyacente") 
+        formula.place(rely=0.30,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="cos α =") 
+        formula.place(rely=0.32,relx=0.30,anchor=W)
 
-    e2 = Entry(main_frame,font=("Arial black", 14))
-    e2.place(rely=0.42,relx=0.78,relheight=0.06,anchor=E)
+        # Crear entradas y etiquetas
+        label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo Adyacente")
+        label1.place(rely=0.50,relx=0.22,anchor=W)
 
-    e3 = Entry(main_frame,font=("Arial black", 14))
-    e3.place(rely=0.57,relx=0.78,relheight=0.06,anchor=E)
-    e3.config()
+        label2 = Label(main_frame,font=("Arial black", 14), text="Hipotenusa")
+        label2.place(rely=0.60,relx=0.22,anchor=W)
 
+        label3 = Label(main_frame,font=("Arial black", 14), text="Coseno")
+        label3.place(rely=0.70,relx=0.22,anchor=W)
 
-    # Crear botones
-    bt_calculate = Button(main_frame, text="Calcular Coseno",cursor="hand2", command=calculate_coseno,
-                        font=("Arial black", 18), bg="#FF6F61", fg='white')
-    bt_calculate.place(rely=0.75,relx=0.50, anchor=CENTER) 
+        e1 = Entry(main_frame,font=("Arial black", 14))
+        e1.place(rely=0.50,relx=0.78,relheight=0.06,anchor=E)
+
+        e2 = Entry(main_frame,font=("Arial black", 14))
+        e2.place(rely=0.60,relx=0.78,relheight=0.06,anchor=E)
+
+        e3 = Entry(main_frame,font=("Arial black", 14))
+        e3.place(rely=0.70,relx=0.78,relheight=0.06,anchor=E)
+        e3.config()
+
+        # Crear botones
+        bt_calculate = Button(main_frame, text="Calcular Coseno",cursor="hand2", command=calcular_coseno_sin_angulo,
+                            font=("Arial black", 18), bg="#FF6F61", fg='white')
+        bt_calculate.place(rely=0.85,relx=0.50, anchor=CENTER) 
+
+    def coseno_con_angulo_op():
+        lb = Label(main_frame, text="COSENO", font=("Arial black",25))
+        lb.place(relx=0.50,rely=0.10,anchor=CENTER)
+
+        def calcular_coseno_con_angulo():
+            anguloCoseno = float(e1.get())
+            coseno = math.cos(math.radians(anguloCoseno))
+            e2.delete(0, "end")
+            e2.insert(END, f'{round(coseno,5)}')
+
+        formula = Label(main_frame,font=("Arial black", 25), text="Hipotenusa") 
+        formula.place(rely=0.40,relx=0.465,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="________________") 
+        formula.place(rely=0.36,relx=0.435,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Adyacente") 
+        formula.place(rely=0.35,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="cos α =") 
+        formula.place(rely=0.37,relx=0.30,anchor=W)
+
+        # Crear entradas y etiquetas
+        label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo del Coseno")
+        label1.place(rely=0.60,relx=0.22,anchor=W)
+
+        label2 = Label(main_frame,font=("Arial black", 14), text="Coseno")
+        label2.place(rely=0.70,relx=0.22,anchor=W)
+
+        e1 = Entry(main_frame,font=("Arial black", 14))
+        e1.place(rely=0.60,relx=0.78,relheight=0.06,anchor=E)
+
+        e2 = Entry(main_frame,font=("Arial black", 14))
+        e2.place(rely=0.70,relx=0.78,relheight=0.06,anchor=E)
+        e2.config()
+
+        # Crear botones
+        bt_calculate = Button(main_frame, text="Calcular Coseno",cursor="hand2", command=calcular_coseno_con_angulo,
+                            font=("Arial black", 18), bg="#FF6F61", fg='white')
+        bt_calculate.place(rely=0.85,relx=0.50, anchor=CENTER) 
 
 
 def tangente_page():
     lb = Label(main_frame, text="TANGENTE", font=("Arial black",25))
     lb.place(relx=0.50,rely=0.10,anchor=CENTER)
 
-    def calculate_tangente():
-        anguloOpuesto = float(e1.get())
-        anguloAdyacente = float(e2.get())
-        tangente = anguloOpuesto/anguloAdyacente
-        e3.delete(0, "end")
-        e3.insert(END, f'{tangente}')
+    seno_angulo_btn = Button(main_frame, text="CALCULAR TANGENTE SIN ÁNGULO", font=("Arial black", 18), bg="#FF6F61", fg='white',
+                    cursor="hand2",command=lambda: pages(tangente_sin_angulo_op))
+    seno_angulo_btn.place(rely=0.35,relx=0.50,width=520, height=100, anchor=CENTER)
 
-    # Crear entradas y etiquetas
-    label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo Opuesto")
-    label1.place(rely=0.27,relx=0.22,anchor=W)
+    seno_razon_btn = Button(main_frame, text="CALCULAR TANGENTE CON ÁNGULO", font=("Arial black", 18), bg="#FF6F61", fg='white',
+                    cursor="hand2",command=lambda: pages(tangente_con_angulo_op))
+    seno_razon_btn.place(rely=0.60,relx=0.50,width=520, height=100, anchor=CENTER)
+    
 
-    label2 = Label(main_frame,font=("Arial black", 14), text="Ángulo Adyacente")
-    label2.place(rely=0.42,relx=0.22,anchor=W)
 
-    label3 = Label(main_frame,font=("Arial black", 14), text="Tangente")
-    label3.place(rely=0.57,relx=0.22,anchor=W)
+    def tangente_sin_angulo_op():
+        lb = Label(main_frame, text="TANGENTE", font=("Arial black",25))
+        lb.place(relx=0.50,rely=0.10,anchor=CENTER)
 
-    e1 = Entry(main_frame,font=("Arial black", 14))
-    e1.place(rely=0.27,relx=0.78,relheight=0.06,anchor=E)
+        def calcular_tangente_sin_angulo():
+            catetoOpuesto = float(e1.get())
+            hipotenusa = float(e2.get())
+            tangente = catetoOpuesto/hipotenusa
+            e3.delete(0, "end")
+            e3.insert(END, f'{round(tangente,2)}')
 
-    e2 = Entry(main_frame,font=("Arial black", 14))
-    e2.place(rely=0.42,relx=0.78,relheight=0.06,anchor=E)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Opuesto") 
+        formula.place(rely=0.35,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="________________") 
+        formula.place(rely=0.31,relx=0.435,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Adyacente") 
+        formula.place(rely=0.30,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="tan α =") 
+        formula.place(rely=0.32,relx=0.30,anchor=W)
 
-    e3 = Entry(main_frame,font=("Arial black", 14))
-    e3.place(rely=0.57,relx=0.78,relheight=0.06,anchor=E)
-    e3.config()
+        # Crear entradas y etiquetas
+        label1 = Label(main_frame,font=("Arial black", 14), text="Cateto Opuesto")
+        label1.place(rely=0.50,relx=0.22,anchor=W)
 
-    # Crear botones
-    bt_calculate = Button(main_frame, text="Calcular Tangente",cursor="hand2", command=calculate_tangente,
-                        font=("Arial black", 18), bg="#FF6F61", fg='white')
-    bt_calculate.place(rely=0.75,relx=0.50, anchor=CENTER) 
+        label2 = Label(main_frame,font=("Arial black", 14), text="Cateto Adyacente")
+        label2.place(rely=0.60,relx=0.22,anchor=W)
+
+        label3 = Label(main_frame,font=("Arial black", 14), text="Tangente")
+        label3.place(rely=0.70,relx=0.22,anchor=W)
+
+        e1 = Entry(main_frame,font=("Arial black", 14))
+        e1.place(rely=0.50,relx=0.78,relheight=0.06,anchor=E)
+
+        e2 = Entry(main_frame,font=("Arial black", 14))
+        e2.place(rely=0.60,relx=0.78,relheight=0.06,anchor=E)
+
+        e3 = Entry(main_frame,font=("Arial black", 14))
+        e3.place(rely=0.70,relx=0.78,relheight=0.06,anchor=E)
+        e3.config()
+
+        # Crear botones
+        bt_calculate = Button(main_frame, text="Calcular Tangente",cursor="hand2", command=calcular_tangente_sin_angulo,
+                            font=("Arial black", 18), bg="#FF6F61", fg='white')
+        bt_calculate.place(rely=0.85,relx=0.50, anchor=CENTER) 
+
+    def tangente_con_angulo_op():
+        lb = Label(main_frame, text="TANGENTE", font=("Arial black",25))
+        lb.place(relx=0.50,rely=0.10,anchor=CENTER)
+
+        def calcular_tangente_con_angulo():
+            anguloTangente = float(e1.get())
+            tangente = math.sin(math.radians(anguloTangente))
+            e2.delete(0, "end")
+            e2.insert(END, f'{round(tangente,2)}')
+
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Opuesto") 
+        formula.place(rely=0.35,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="________________") 
+        formula.place(rely=0.31,relx=0.435,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="Cateto Adyacente") 
+        formula.place(rely=0.30,relx=0.43,anchor=W)
+        formula = Label(main_frame,font=("Arial black", 25), text="tan α =") 
+        formula.place(rely=0.32,relx=0.30,anchor=W)
+
+        # Crear entradas y etiquetas
+        label1 = Label(main_frame,font=("Arial black", 14), text="Ángulo de la Tangente")
+        label1.place(rely=0.60,relx=0.22,anchor=W)
+
+        label2 = Label(main_frame,font=("Arial black", 14), text="Tangente")
+        label2.place(rely=0.70,relx=0.22,anchor=W)
+
+        e1 = Entry(main_frame,font=("Arial black", 14))
+        e1.place(rely=0.60,relx=0.78,relheight=0.06,anchor=E)
+
+        e2 = Entry(main_frame,font=("Arial black", 14))
+        e2.place(rely=0.70,relx=0.78,relheight=0.06,anchor=E)
+        e2.config()
+
+        # Crear botones
+        bt_calculate = Button(main_frame, text="Calcular Tangente",cursor="hand2", command=calcular_tangente_con_angulo,
+                            font=("Arial black", 18), bg="#FF6F61", fg='white')
+        bt_calculate.place(rely=0.85,relx=0.50, anchor=CENTER) 
 
 
 def delete_pages():
